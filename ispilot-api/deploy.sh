c8876bfb65126ec95da772961bcb48f6a3a9915e
Sebastian Ulises Duque Rodriguez <sduque@Falabella.cl>
2026-08-28 16:22:45 -0400

    change sa account deploy

--- ispilot-api/deploy.sh
+++ ispilot-api/deploy.sh
@@ -6,7 +6,6 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
-
 # Configuration
 PROJECT_ID="${PROJECT_ID:-corp-stro-salesinventory-prod}"
 REGION="${REGION:-us-central1}"
